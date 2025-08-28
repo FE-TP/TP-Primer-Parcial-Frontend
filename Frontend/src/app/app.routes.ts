d890bba96014e46a69a32ac53c221ae15f3b066e
EJFR009 <ejfr09@fpuna.edu.py>
2025-08-28 16:01:11 -0300

    Revert "Modulo 3 V1"
    This reverts commit a9286c541ac444480574d3eaafe9f765d990bc5d.

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,13 +1,12 @@
 import { Routes } from '@angular/router';
 import { RecepcionComponent } from './recepcion/recepcion.component';
-import { JaulasComponent } from './jaulas/jaulas';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/recepcion', pathMatch: 'full' },
   { path: 'recepcion', component: RecepcionComponent },
-  { path: 'jaulas', component: JaulasComponent },
   // Aquí agregarás las rutas de tus compañeros para los otros módulos
   // { path: 'proveedores', component: ProveedoresComponent },
   // { path: 'productos', component: ProductosComponent },
+  // { path: 'jaulas', component: JaulasComponent },
   // { path: 'reservas', component: ReservasComponent },
 ];
